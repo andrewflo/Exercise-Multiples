672b8a5818075d584d04c76f171a2b77b0be8b37
Andrew Flores <andrewjflores1@gmail.com>
2016-04-10 18:16:47 -0700

    Initial commit

--- Exercise-Multiples/ViewController.swift
+++ Exercise-Multiples/ViewController.swift
@@ -10,16 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Properties
+    var maxResult = 0
+    var multiple = 0
+    var currentNumber = 0
+    var result = 0
+    
+    // Outlets
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var multipleTextField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var multipleLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    // Actions
+    @IBAction func playButtonPressed(sender: UIButton!) {
+        if multipleTextField.text != nil && multipleTextField.text != "" {
+            logoImage.hidden = true
+            multipleTextField.hidden = true
+            playButton.hidden = true
+            multipleLabel.hidden = false
+            addButton.hidden = false
+            
+            multiple = Int(multipleTextField.text!)!
+            maxResult = multiple * 10
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addButtonPressed(sender: UIButton!) {
+        if !isGameOver() {
+            currentNumber += multiple
+            result = currentNumber + multiple
+        
+            updateMultipleLabel()
+        } else {
+            restartGame()
+        }
     }
-
+    
+    // Functions
+    func isGameOver() -> Bool {
+        return result >= maxResult
+    }
+    
+    func restartGame() {
+        logoImage.hidden = false
+        multipleTextField.hidden = false
+        playButton.hidden = false
+        multipleLabel.hidden = true
+        addButton.hidden = true
+        
+        currentNumber = 0
+        multiple = 0
+        result = 0
+        
+        multipleTextField.text = ""
+        multipleLabel.text = "Press Add to add!"
+    }
+    
+    func updateMultipleLabel() {
+        multipleLabel.text = "\(currentNumber) + \(multiple) = \(result)"
+    }
+    
+    // ViewController Methods
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
 
 }
 
